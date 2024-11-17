7db3ec7b9a2e5ffd6f81ece2a8941bfef3e78f78
Marlon <marlon.john@hotmail.de>
2024-11-17 16:49:58 +0100

    more features

---
+++ src/OffiUtils/DateTimeUtils.Ext.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace OffiUtils;
+
+public static class DateTimeUtils
+{
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static DateTime Trim(this DateTime date, long roundTicks)
+	{
+		return new DateTime(date.Ticks - date.Ticks % roundTicks, date.Kind);
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static DateTimeOffset Trim(this DateTimeOffset date, long roundTicks)
+	{
+		return new DateTimeOffset(date.Ticks - date.Ticks % roundTicks, date.Offset);
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static TimeOnly Trim(this TimeOnly time, long roundTicks)
+	{
+		return new TimeOnly(time.Ticks - time.Ticks % roundTicks);
+	}
+}
