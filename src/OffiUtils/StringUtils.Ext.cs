ac3515f7b5f26c94562980d4fd972f89e6db5d4c
Marlon <marlon.john@hotmail.de>
2025-11-13 13:44:56 +0100

    added ToPooledString extension

--- src/OffiUtils/StringUtils.Ext.cs
+++ src/OffiUtils/StringUtils.Ext.cs
@@ -1,13 +1,15 @@
-﻿using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
+
+using CommunityToolkit.HighPerformance.Buffers;
 
 namespace OffiUtils;
 
 public static partial class StringUtils
 {
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static ref char GetRawData(this string value) => ref MemoryMarshal.GetReference(value.AsSpan());
 
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static Span<char> GetSpan(this string value) => MemoryMarshal.CreateSpan(ref GetRawData(value), value.Length);
+
+	public static string ToPooledString(this ReadOnlySpan<char> value, StringPool? pool = null)
+		=> (pool ?? StringPool.Shared).GetOrAdd(value);
 }
